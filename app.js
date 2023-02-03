cf889a98d006e078dc813a5d936029a0371ccaed
Mihai Serbanescu <102154186+serbanescu-mihai@users.noreply.github.com>
2023-02-03 20:10:09 +0100

    final

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// navigation
+
 const toggleButton = document.querySelector(".toggle-button");
 const navigation = document.querySelector(".navigation");
 const links = document.querySelectorAll(".navigation-link");
@@ -8,5 +10,26 @@ toggleButton.addEventListener("click", () => {
 links.forEach((e) => {
   e.addEventListener("click", () => {
     navigation.classList.remove("hidden");
+  });
+});
+
+// galery
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
   });
 });
