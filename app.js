b47264557f6a1b287ab774503048cc3162e26296
Mihai Serbanescu <102154186+serbanescu-mihai@users.noreply.github.com>
2023-02-03 16:32:02 +0100

    navigation

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const toggleButton = document.querySelector(".toggle-button");
+const navigation = document.querySelector(".navigation");
+const links = document.querySelectorAll(".navigation-link");
+
+toggleButton.addEventListener("click", () => {
+  navigation.classList.toggle("hidden");
+});
+links.forEach((e) => {
+  e.addEventListener("click", () => {
+    navigation.classList.remove("hidden");
+  });
+});
